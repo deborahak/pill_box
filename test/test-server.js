859332e2a7ae03101c512ab0ce9a6b8b1d0f4d28
deborahak <deborahkain@mac.com>
2017-07-11 00:04:19 -0700

    testing update

--- test/test-server.js
+++ test/test-server.js
@@ -8,64 +8,93 @@ const should = chai.should();
 chai.use(chaiHttp);
 
 describe('Medications', function() {
-    before(function() {
-        return runServer(databaseUrl = "mongodb://localhost/pill_box_test");
-    });
-    after(function() {
-        mongoose.connection.db.dropDatabase(); /// delete the database
-        return closeServer();
-    });
-    it('should create a new user on POST', function() {
-        const newUser = { username: 'deborah', password: 'lplplp' }
-        return chai.request(app)
-            .post('/api/signup')
-            .send(newUser)
-            .then(function(res) {
-                res.should.have.status(201);
-                res.should.be.json;
-                res.body.should.be.a('object');
-                res.body.should.include.keys('error', 'message');
+            before(function() {
+                return runServer(databaseUrl = "mongodb://localhost/pill_box_test");
             });
-    });
-
-    it('should create a new medication on POST', function() {
-        /// how do we get the token?
-
-       chai.request(app)
-            .post('/api/authenticate')
-            .send({ username: 'deborah', password: 'lplplp' })
-            .then(function(res) {
-            	console.log(res.body);
+            after(function() {
+                mongoose.connection.db.dropDatabase(); /// delete the database
+                return closeServer();
+            });
+            it('should create a new user on POST', function() {
+                const newUser = { username: 'deborah', password: 'lplplp' }
                 return chai.request(app)
-                    .post('/api/medicationns?token='+ res.body.token)
-                    .send({ name: 'asprin', dose: 'one', timing: 'day', description: 'red' })
+                    .post('/api/signup')
+                    .send(newUser)
                     .then(function(res) {
-                    	console.log(res.body, 'anything');
                         res.should.have.status(201);
                         res.should.be.json;
                         res.body.should.be.a('object');
-                        res.body.should.include.keys('name', 'dose', 'timing', 'description');                        
-                        res.body.name.should.equal('asprin');
+                        res.body.should.include.keys('error', 'message');
+                    });
+            });
+
+            it('should create a new medication on POST', function() {
+                /// how do we get the token?
+
+                return chai.request(app)
+                    .post('/api/authenticate')
+                    .send({ username: 'deborah', password: 'lplplp' })
+                    .then(function(res) {
+                        return chai.request(app)
+                            .post('/api/medications?token=' + res.body.token)
+                            .send({ name: 'asprin', dose: 'one', timing: 'day', description: 'red' })
+                            .then(function(res) {
+                                res.should.have.status(201);
+                                res.should.be.json;
+                                res.body.should.be.a('object');
+                                res.body.should.include.keys('name', 'dose', 'timing', 'description');
+                                res.body.name.should.equal('asprin');
+                            })
+                            .catch(function(err) {
+                                throw err;
+                            })
                     })
-                    // .catch(function(err){
-                    // 	throw err;
-                    // })
-            })
-            .catch(function(err){
-            	throw err;
-            })
-    });
+                    .catch(function(err) {
+                        throw err;
+                    })
+            });
 
+            it('should delete medications on DELETE', function() {
+                return chai.request(app)
+                    .post('/api/authenticate')
+                    .send({ username: 'deborah', password: 'lplplp' })
+                    .then(function(res) {
+                        const { token } = res.body; // const token = res.body.token.
+                        return chai.request(app)
+                            .get(`/api/medications?token=${token}`)
+                            .then(function(res) {
+                                return chai.request(app)
+                                    .delete(`/api/medications/${res.body.medications[0]._id}?token=${token}`)
+                                    .then(function(res) {
+                                        res.should.have.status(200)
+                                    })
+                            })
+                    });
+            });
+            it('should update medications on PUT', function() {
+                return chai.request(app)
+                    .post('/api/authenticate')
+                    .send({ username: 'deborah', password: 'lplplp' })
+                    .then(function(res) {
+                        const { token } = res.body;
+                        return chai.request(app)
+                            .get(`/api/medications?token=${token}`)
+                            .then(function(res) {
+                                return chai.request(app)
+                                    .put(`/api/medications/${res.body.medications[0]._id}?token=${token}`)
+                                    .send({ name: 'tylenol', dose: 'two', timing: 'morning', description: 'orange' })
+                                    .then(function(res) {
+                                        res.should.have.status(200)
+                                        res.should.be.json;
+                                        res.body.should.be.a('object');
+                                        res.body.should.include.keys('name', 'dose', 'timing', 'description');
 
-    // it('should delete medications on DELETE', function() {
-    //     return chai.request(app)
-    //         .get('/api/medications')
-    //         .then(function(res) {
-    //             return chai.request(app)
-    //                 .delete(`/api/medications/${res.body[0].id}`);
-    //         })
-    //         .then(function(res) {
-    //             res.should.have.status(204);
-    //         });
-    // });
-});
+                                    })
+                                    .catch(function(err) {
+                                        throw (err);
+                                    })
+                            });
+
+                    });
+            });
+        });
