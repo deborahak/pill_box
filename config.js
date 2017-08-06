be255bdc6f467311a03667ef9ffd18278f15a838
deborahak <deborahkain@mac.com>
2017-08-06 08:39:20 -0700

    debugging

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
 exports.DATABASE_URL = process.env.DATABASE_URL || global.DATABASE_URL || 'mongodb://localhost/pill_box';
-exports.TEST_DATABASE_URL = (
-	process.env.TEST_DATABASE_URL ||  "mongodb://localhost/pill_box_test")
+exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL;
 exports.jwt_secret = 'SunnySandy*';	
 exports.PORT = process.env.PORT || 8080;
