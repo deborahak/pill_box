14f827150e85c974ca3608944d975435ff7f8493
Anna Mendoza <annamendoza.curve@gmail.com>
2018-07-01 13:44:15 -0700

    updating the error on the internal forms, one down, one more to go

--- public/js/add_meds.js
+++ public/js/add_meds.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
 		console.log(event);
 		var medName = $('#medName').val();
 		var medDose = $('#medDose').val();
-		var medTime = $('#medTime').val();
+		//var medTime = $('#medTime').val();
 		var medDescription = $('#medDescription').val();
 
 		$.ajax({
@@ -14,6 +14,7 @@ $(document).ready(function(){
 			data: { name: medName, dose: medDose, timing: [medTime], description: medDescription},
 			dataType:'json'
 		}).done(function(data){
+				console.log(data);
 				if (data.message){
                 console.log('error');
                 $('.error').html(data.message);
@@ -21,14 +22,14 @@ $(document).ready(function(){
 
                 var $inputs = $(".add_med input");
                 $inputs.on("input", function() {
-                    var $filled = $inputs.filter(function() {return this.value.trim().length > 0;});
-                $('#logged').toggleClass('has-error', $filled.length > 0);
-                $('#addMed').click(function() {
+                  var $filled = $inputs.filter(function() {return this.value.trim().length > 0;});
+                	$('#logged').toggleClass('has-error', $filled.length > 0);
+                	$('#addMed').click(function() {
                     $inputs.val('').trigger('input');
-                });
+                	});
                 });
             } else {
-							location.href = '/medications';
+							location.href = '/schedule';
             }
 			})
 	})
